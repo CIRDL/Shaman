b0365c076bd98af0d16bb773398c077883ef0101
CIRDL <cesar.i.ramirez-1@ou.edu>
2023-05-18 11:18:54 -0500

    Major updates for the optimization process

--- optimization.py
+++ optimization.py
@@ -1,9 +1,8 @@
 # @author Cesar Ramirez
 # Optimization model for choosing class schedule
-
-
+import gurobipy
 from gurobipy import *
-from numpy import max, array
+from num2words import *
 
 # READ DATA FROM EXCEL
 import openpyxl as opxl
@@ -47,17 +46,19 @@ while ws.cell(row=row+1, column=1).value:
     row += 1
 
 # Populating parameter prerequisites prereq
+# Indexed by class c first, then prerequisites index i from class order C
 row = 1
 col = 1
 ind_pre = []
-while ws.cell(row=row, column=col+1).value:
+while ws.cell(row=row+1, column=col).value:
     ind_pre = []
-    while ws.cell(row=row+1, column=col+1).value or ws.cell(row=row+1, column=col+1).value == 0:
+    while ws.cell(row=row, column=col+1).value or ws.cell(row=row, column=col+1).value == 0:
         ind_pre.append(ws.cell(row=row+1, column=col+1).value)
-        row += 1
-    prereq[C[col - 1]] = ind_pre
-    row = 1
-    col += 1
+        col += 1
+    prereq[C[row - 1]] = ind_pre
+    col = 1
+    row += 1
+
 
 # Populating parameter hours h
 ws = doc["ISE"]
@@ -87,72 +88,57 @@ while ws.cell(row=row+1, column=col).value:
 model = Model("Schedule")
 model.setParam(GRB.Param.OutputFlag, 0)
 
-# Variable - sched is indexed as semester x classes
-# This means that the first index will be the semester (int) and the second term will be class name (string)
+# Decision Variables
+
+# sched - first index will be the semester (int) and the second term will be class name (string)
+# 1 for taking class at class c at semester s, 0 otherwise
 sched = {s: model.addVars(C, name="x", vtype=GRB.BINARY) for s in S}
 
-# TODO - need to fix everything since I switched the order of the indexes
-
-# Somehow trying to find the max and min semesters by hour for the objective function
-semester_hours_list = []
-for s in S:
-    semester_hours_list.append(sched[s].sum())
-
-semester_hours = array(semester_hours_list)
+# bound - single integer variable that represents the highest number of hours that will be required in a semester
+# Implicitly accounts for the min and max number of hours per semester to be a full time student
+# bounds = model.addVars(S, vtype=GRB.INTEGER, lb=MIN_HOURS, ub=MAX_HOURS, name="bounds")
+bound = model.addVar(vtype=GRB.INTEGER, lb=MIN_HOURS, ub=MAX_HOURS, name="bound")
 
 # Constraints
 
-# Meeting credit hour requirements
-model.addConstrs(quicksum(sched[i][j]*hrs[i] for j in C) >= MIN_HOURS for i in S)
-model.addConstrs(quicksum(sched[i][j]*hrs[i] for j in C) <= MAX_HOURS for i in S)
+# Configuring bound constraint
+model.addConstrs(quicksum(sched[s][c]*hrs[c] for c in C) <= bound for s in S)
+
+# Maintain minimum number of hours for full time student constraint
+model.addConstr(bound <= MAX_HOURS)
+
+# model.addConstrs(quicksum(sched[s][c] for c in C) >= MIN_HOURS for s in S)
 
 # Take each required class
-model.addConstrs(quicksum(sched[i][j] for j in C) == 1 for i in S)
+model.addConstrs(quicksum(sched[s][c] for s in S) == 1 for c in C)
 
 # Availability constraint
-model.addConstrs(sched[i][j] <= avai[i][j] for i in S for j in C)
-
-model.update()
-
-# max_hrs = 0
-# max_sem = 0
-# for s in S:
-#     cur_hrs = 0
-#     for c in C:
-#         cur_hrs += sched[c][s]
-#         print(sched[c][s])
-#     if max_hrs < cur_hrs:
-#         max_sem = s
-#         max_hrs = cur_hrs
-# print(max_sem)
-print(sched)
-
-# ----------------------------- Blocked out code
+model.addConstrs(sched[i][j] <= avai[j][i] for i in S for j in C)
 
 # Prerequisite constraint
-# model.addConstrs(quicksum(sched[C[k]][sp] for sp in S) >= prereq[i][k] for k in range(len(C)) for i in C for j in S)
+# model.addConstrs(quicksum(sched[k][C[p]] for k in range(s)) <= prereq[c][p] for p in range(len(C))
+#                  for c in C for s in S)
 
 # Objective function
-# model.setObjective(quicksum(quicksum(quicksum(sched[i][j]*hrs[i])/len(S) for i in C) for j in S
-#                             - quicksum(sched[i][k]*hrs[i]) for i in C) for k in S)
+model.setObjective(bound, sense=GRB.MINIMIZE)
+model.setParam("OutputFlag", 0)
 
-# # Tweak model
-# model.modelSense = GRB.MINIMIZE
-# model.update()
-# model.setParam("OutputFlag", 0)
-#
-# # Optimize model
-# model.optimize()
-#
-# scrape = []
-#
-# # Printing outputs
-# if model.status == GRB.OPTIMAL:
-#     print("\nOptimal value:", model.objVal)
-#     print("--- Quantities---")
-#     for v in model.getVars():
-#         if v.x is True and v.varName[0] == "x":
-#             print("%s: %g" % (v.varName, v.x))
-#             scrape.append(v.varName[8:14])
-# else:
-#     print("No solution")
+model.update()
+model.optimize()
+
+hour_count = 0
+
+# Printing outputs
+if model.status == GRB.OPTIMAL:
+    print("\nMaximum number of hours required:", model.objVal)
+    print("---Schedule---")
+    for s in S:
+        print(f"{num2words(s+1, to='ordinal').capitalize()} semester:")
+        for c in C:
+            if sched[s][c] is True:
+                hour_count += hrs[c]
+                print(f"{c}: {hrs[c]}")
+        print("Total hours: ", hour_count)
+    hour_count = 0
+else:
+    print("Not possible :(")
